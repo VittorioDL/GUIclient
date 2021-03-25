09536f7f5756149152cb5de78fbbbda9b36fb98a
Vittorio <victor03.VDL@gmail.com>
2021-03-25 13:12:33 +0100

    Finestra vuota

--- GUI.java
+++ GUI.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 
 public class GUI extends JFrame implements ActionListener, KeyListener, MouseListener
 {
-    int nSprite = (int)(Math.random()*7+3);
-    
-    ArrayList<Point> posizioni; 
-    ArrayList<Point> destinazioni;
-    
     Timer t = new Timer(10, this);
     public GUI()
     {  
@@ -21,54 +16,10 @@ public class GUI extends JFrame implements ActionListener, KeyListener, MouseLis
         
         t.setActionCommand("TIMER");
         
-        //Creiamo gli sprites in modo random
+       
         
-        
-        //Posizioniamo e visualizziamo gli sprites in modo random
-        posizioni = new ArrayList<Point>(nSprite); 
-        destinazioni = new ArrayList<Point>(nSprite); 
-        
-        for(int i = 0; i < nSprite; i++)
-        {
-            int x = (int)(Math.random()*(500))+50;
-            int y = (int)(Math.random()*(700));
-            
-            int x1 = (int)(Math.random()*(500))+50;
-            int y1 = (int)(Math.random()*(700));
-            
-            posizioni.add(new Point(x, y));
-            destinazioni.add(new Point(x1, y1));
-            
-            
-        }
+       
         t.start();
-    }
-    
-    public void spostamentoLabel(int currentX, int currentY, int x, int y, JLabel testo) 
-    {   
-        if(currentX<x)
-        {
-            currentX++;
-            testo.setLocation(currentX, currentY);
-        }
-                
-        else if(currentX>x)
-        {
-            currentX--;
-            testo.setLocation(currentX, currentY);
-        }
-                
-        if(currentY<y)
-        {
-            currentY++;
-            testo.setLocation(currentX, currentY);
-        }
-                
-        else if(currentY>y)
-        {
-            currentY--;
-            testo.setLocation(currentX, currentY);
-        }
     }
     
     public void actionPerformed(ActionEvent e)
